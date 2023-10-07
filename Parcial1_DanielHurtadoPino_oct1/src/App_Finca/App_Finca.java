3a141ca918c12dee24dcce97c4c4ca87431ba3d6
nohemi125 <nohemy_morelo69222@elpoli.edu.co>
2023-10-07 15:54:39 -0500

    Boton nuevo y registrar
    Boton nuevo y registrar

--- Parcial1_DanielHurtadoPino_oct1/src/App_Finca/App_Finca.java
+++ Parcial1_DanielHurtadoPino_oct1/src/App_Finca/App_Finca.java
@@ -48,10 +48,11 @@ public class App_Finca extends javax.swing.JFrame {
         jLabel11 = new javax.swing.JLabel();
         jPanel9 = new javax.swing.JPanel();
         btn_registrar = new javax.swing.JButton();
-        btn_guardar = new javax.swing.JButton();
+        btn_nuevo = new javax.swing.JButton();
         btn_salir = new javax.swing.JButton();
         btn_consultar = new javax.swing.JButton();
         btn_actualizar = new javax.swing.JButton();
+        btn_guardar1 = new javax.swing.JButton();
 
         jButton5.setBackground(new java.awt.Color(204, 204, 204));
         jButton5.setFont(new java.awt.Font("Segoe UI Symbol", 0, 12)); // NOI18N
@@ -310,10 +311,15 @@ public class App_Finca extends javax.swing.JFrame {
             }
         });
 
-        btn_guardar.setBackground(new java.awt.Color(204, 204, 204));
-        btn_guardar.setFont(new java.awt.Font("Segoe UI Symbol", 0, 12)); // NOI18N
-        btn_guardar.setText("Guardar");
-        btn_guardar.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        btn_nuevo.setBackground(new java.awt.Color(204, 204, 204));
+        btn_nuevo.setFont(new java.awt.Font("Segoe UI Symbol", 0, 12)); // NOI18N
+        btn_nuevo.setText("Nuevo");
+        btn_nuevo.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        btn_nuevo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_nuevoActionPerformed(evt);
+            }
+        });
 
         btn_salir.setBackground(new java.awt.Color(204, 204, 204));
         btn_salir.setFont(new java.awt.Font("Segoe UI Symbol", 0, 12)); // NOI18N
@@ -324,11 +330,21 @@ public class App_Finca extends javax.swing.JFrame {
         btn_consultar.setFont(new java.awt.Font("Segoe UI Symbol", 0, 12)); // NOI18N
         btn_consultar.setText("Consultar");
         btn_consultar.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        btn_consultar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_consultarActionPerformed(evt);
+            }
+        });
 
         btn_actualizar.setBackground(new java.awt.Color(204, 204, 204));
         btn_actualizar.setFont(new java.awt.Font("Segoe UI Symbol", 0, 12)); // NOI18N
         btn_actualizar.setText("Actualizar");
         btn_actualizar.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+
+        btn_guardar1.setBackground(new java.awt.Color(204, 204, 204));
+        btn_guardar1.setFont(new java.awt.Font("Segoe UI Symbol", 0, 12)); // NOI18N
+        btn_guardar1.setText("Guardar");
+        btn_guardar1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
 
         javax.swing.GroupLayout jPanel9Layout = new javax.swing.GroupLayout(jPanel9);
         jPanel9.setLayout(jPanel9Layout);
@@ -337,7 +353,7 @@ public class App_Finca extends javax.swing.JFrame {
             .addGroup(jPanel9Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel9Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(btn_guardar, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 102, Short.MAX_VALUE)
+                    .addComponent(btn_nuevo, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 102, Short.MAX_VALUE)
                     .addComponent(btn_salir, javax.swing.GroupLayout.DEFAULT_SIZE, 102, Short.MAX_VALUE)
                     .addComponent(btn_registrar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
@@ -351,15 +367,20 @@ public class App_Finca extends javax.swing.JFrame {
                     .addContainerGap()
                     .addComponent(btn_actualizar, javax.swing.GroupLayout.DEFAULT_SIZE, 102, Short.MAX_VALUE)
                     .addContainerGap()))
+            .addGroup(jPanel9Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel9Layout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(btn_guardar1, javax.swing.GroupLayout.DEFAULT_SIZE, 102, Short.MAX_VALUE)
+                    .addContainerGap()))
         );
         jPanel9Layout.setVerticalGroup(
             jPanel9Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel9Layout.createSequentialGroup()
                 .addGap(23, 23, 23)
                 .addComponent(btn_registrar, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(164, 164, 164)
-                .addComponent(btn_guardar, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btn_nuevo, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(66, 66, 66)
                 .addComponent(btn_salir, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
             .addGroup(jPanel9Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -372,6 +393,11 @@ public class App_Finca extends javax.swing.JFrame {
                     .addGap(162, 162, 162)
                     .addComponent(btn_actualizar, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addContainerGap(247, Short.MAX_VALUE)))
+            .addGroup(jPanel9Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel9Layout.createSequentialGroup()
+                    .addContainerGap(235, Short.MAX_VALUE)
+                    .addComponent(btn_guardar1, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(174, 174, 174)))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -448,9 +474,27 @@ public class App_Finca extends javax.swing.JFrame {
         else{
             trabajador[int_posiciones] = new Cls_trabajadores(txt_identificacion.getText(), txt_nombre.getText(), txt_apellidos.getText(), txt_sexo.getText(), txt_contacto.getText(), txt_correo.getText(), Integer.parseInt(txt_direccion.getText()), Integer.parseInt(txt_salario.getText()));
                 int_posiciones+=1;
-                JOptionPane.showMessageDialog(null, "Paciente registrado","Registro",JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showMessageDialog(null, "Trabajador registrado con exito","Registro",JOptionPane.INFORMATION_MESSAGE);
         }
     }//GEN-LAST:event_btn_registrarActionPerformed
+
+    private void btn_consultarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_consultarActionPerformed
+        
+    }//GEN-LAST:event_btn_consultarActionPerformed
+    private void fnt_limpiarcontroles(){
+        txt_identificacion.setText("");
+        txt_nombre.setText("");
+        txt_apellidos.setText("");
+        txt_sexo.setText("");
+        txt_contacto.setText("");
+        txt_correo.setText("");
+        txt_direccion.setText("");
+        txt_salario.setText("");
+        txt_identificacion.requestFocus();
+    }
+    private void btn_nuevoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_nuevoActionPerformed
+       fnt_limpiarcontroles();
+    }//GEN-LAST:event_btn_nuevoActionPerformed
     
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -487,7 +531,8 @@ public class App_Finca extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btn_actualizar;
     private javax.swing.JButton btn_consultar;
-    private javax.swing.JButton btn_guardar;
+    private javax.swing.JButton btn_guardar1;
+    private javax.swing.JButton btn_nuevo;
     private javax.swing.JButton btn_registrar;
     private javax.swing.JButton btn_salir;
     private javax.swing.JButton jButton5;
